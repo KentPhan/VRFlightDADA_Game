fdf3f7b29a11313980825f0b43e99374ae193cd3
KentPhan <u0734504@ad.utah.edu>
2018-10-16 17:56:24 -0600

    Added basic player with forward movement and flapping with space bar

---
+++ VRPaintFight/Assets/Scripts/Entities/PlayerEntity.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerEntity : MonoBehaviour
+{
+    public float MaxFlapForce = 10.0f;
+    public float ConstantForwardSpeed = 10.0f;
+
+
+
+
+
+    private Rigidbody m_RigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        this.m_RigidBody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 l_leftPosition = InputTracking.GetLocalPosition((XRNode.LeftHand));
+        //Vector3 l_rightPosition = InputTracking.GetLocalPosition((XRNode.RightHand));
+
+        if (Input.GetButtonDown("Flap"))
+        {
+            this.m_RigidBody.AddForce(Vector3.up * this.MaxFlapForce, ForceMode.Impulse);
+
+        }
+
+
+        this.m_RigidBody.MovePosition(transform.position + transform.forward * ConstantForwardSpeed * Time.deltaTime);
+        Debug.Log(transform.forward);
+
+
+    }
+
+
+}
