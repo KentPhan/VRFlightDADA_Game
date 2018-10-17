48da67478547b0384ce3b1dd268ef30379747335
KentPhan <u0734504@ad.utah.edu>
2018-10-16 18:58:52 -0600

    Added Manager instances and organized them with Loader

--- VRTeam6Game/Assets/Scripts/Entities/PlayerEntity.cs
+++ VRTeam6Game/Assets/Scripts/Entities/PlayerEntity.cs
@@ -1,38 +1,47 @@
 ﻿using UnityEngine;
 
-public class PlayerEntity : MonoBehaviour
+namespace Assets.Scripts.Entities
 {
-    public float MaxFlapForce = 10.0f;
-    public float ConstantForwardSpeed = 10.0f;
-
-
-
-
-
-    private Rigidbody m_RigidBody;
-
-    // Use this for initialization
-    void Start()
+    /// <summary>
+    /// Player Entity
+    /// </summary>
+    /// <seealso cref="UnityEngine.MonoBehaviour" />
+    public class PlayerEntity : MonoBehaviour
     {
+        public float MaxFlapForce = 10.0f;
+        public float ConstantForwardSpeed = 10.0f;
 
-        this.m_RigidBody = GetComponent<Rigidbody>();
 
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Vector3 l_leftPosition = InputTracking.GetLocalPosition((XRNode.LeftHand));
-        //Vector3 l_rightPosition = InputTracking.GetLocalPosition((XRNode.RightHand));
 
-        if (Input.GetButtonDown("Flap"))
+
+        private Rigidbody m_RigidBody;
+
+        // Use this for initialization
+        void Start()
         {
-            this.m_RigidBody.AddForce(Vector3.up * this.MaxFlapForce, ForceMode.Impulse);
+
+            this.m_RigidBody = GetComponent<Rigidbody>();
 
         }
 
-        this.m_RigidBody.MovePosition(transform.position + transform.forward * ConstantForwardSpeed * Time.deltaTime);
+        // Update is called once per frame
+        void Update()
+        {
+            //Vector3 l_leftPosition = InputTracking.GetLocalPosition((XRNode.LeftHand));
+            //Vector3 l_rightPosition = InputTracking.GetLocalPosition((XRNode.RightHand));
+
+            if (Input.GetButtonDown("Flap"))
+            {
+                this.m_RigidBody.AddForce(Vector3.up * this.MaxFlapForce, ForceMode.Impulse);
+
+            }
+
+            this.m_RigidBody.MovePosition(transform.position + transform.forward * ConstantForwardSpeed * Time.deltaTime);
+        }
+
+
     }
 
-
 }
+
--- VRTeam6Game/Assets/Scripts/Managers/GameManager.cs
+++ VRTeam6Game/Assets/Scripts/Managers/GameManager.cs
@@ -1,16 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+namespace Assets.Scripts.Managers
+{
+    /// <summary>
+    /// Game Manager
+    /// </summary>
+    /// <seealso cref="UnityEngine.MonoBehaviour" />
+    public class GameManager : MonoBehaviour
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        public static GameManager Instance = null;
+
+        public void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+
+            else if (Instance != null)
+            {
+                Destroy(gameObject);
+            }
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
 }
+
+
