4666d658120e9fb78777c29ad74a8e36bd7728b6
LIGHT CHRISTOPHER SEAN BATES <u0683455@ad.utah.edu>
2018-10-31 17:30:33 -0600

    volume/build updates

--- VRTeam6Game/Assets/Scripts/Managers/SoundManager.cs
+++ VRTeam6Game/Assets/Scripts/Managers/SoundManager.cs
@@ -33,10 +33,10 @@ namespace Assets.Scripts.Managers
         // Update is called once per frame
         void Update()
         {
-            var audioClipSpeed = 10.0;
+            //var audioClipSpeed = 10.0;
 
-            var p = GetComponent<Rigidbody>().velocity.magnitude / audioClipSpeed;
-            GetComponent<AudioSource>().pitch = Mathf.Clamp((float)p, (float)0.1, (float)4.0); // p is clamped to same values
+            //var p = GetComponent<Rigidbody>().velocity.magnitude / audioClipSpeed;
+            //GetComponent<AudioSource>().pitch = Mathf.Clamp((float)p, (float)0.1, (float)4.0); // p is clamped to same values
         }
     }
 }
